74eb818dc32f2833fadd682accd8f0984605ecc3
Ken <k3nndroid@gmail.com>
2018-07-23 21:11:39 -0600

    copied code from Johnathan

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -28,37 +28,12 @@ class Signup extends Component {
     this.setState({ signup: event.target.value });
   };
 
-<<<<<<< HEAD
-  // handleFormSubmit = event => {
-  //   event.preventDefault();
-  //   API.getDogsOfBreed(this.state.signup)
-  //     .then(res => {
-=======
-  handleFormSubmit = event => {
-    event.preventDefault();
-    
-    // API.getDogsOfBreed(this.state.signup)
-      // .then(res => {
->>>>>>> master
-  //       if (res.data.status === "error") {
-  //         throw new Error(res.data.message);
-  //       }
-  //       this.setState({ results: res.data.message, error: "" });
-  //     })
-  //     .catch(err => this.setState({ error: err.message }));
-<<<<<<< HEAD
-  // };
-=======
-  };
->>>>>>> master
-  
   handleFormSubmit = (event) => {
     // alert('Your favorite flavor is: ' + this.state.value);
     event.preventDefault();
     console.log(this.state);
 
   }
-
 
   render() {
     return (
