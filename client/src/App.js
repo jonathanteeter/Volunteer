a2e84c28def9ce7270196907ae4f2739b6dfe69e
Ken <k3nndroid@gmail.com>
2018-07-23 19:57:00 -0600

    new codes & a little cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Opportunity from "./pages/Opportunity";
 import Signup from "./pages/Signup";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
@@ -19,9 +16,6 @@ const App = () => (
         <Route exact path="/home" component={Home} />
         <Route exact path="/opportunity" component={Opportunity} />
         <Route exact path="/signup" component={Signup} />
-        <Route exact path="/books" component={Books} />
-        <Route exact path="/books/:id" component={Detail} />
-        <Route exact path="/NoMatch" component={NoMatch} />
       </Wrapper>
       <Footer />
     </div>
