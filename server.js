9871caecfc03bae9d0ccfd604275a53764e4c227
teeterjm58 <jonteeter@outlook.com>
2018-07-27 22:44:09 -0600

    builds project, accounts for extra whitespace added by SET NODE_ENV

--- server.js
+++ server.js
@@ -4,17 +4,25 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production".trim()) {
+  console.log((path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 }
+
+console.log(process.env.NODE_ENV + 'theres a space here!');
 // Add routes, both API and view
 app.use(routes);
+app.get('/*', (req, res) => {
+  console.log(path.join(__dirname, 'client/build'));
+  res.sendFile(path.join(__dirname, 'client/build'));
+});
 
 //Route for scraping
 // Connect to the Mongo DB
